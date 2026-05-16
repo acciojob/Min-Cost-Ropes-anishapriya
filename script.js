6587abf9d98eb0a630bc3a91c61b32cce389a20a
anishapriya <anisharoyanjlina@gmail.com>
2026-05-16 20:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-function mincost(arr, n) {
+function minCost(arr) {
     let cost = 0;
 
     while (arr.length > 1) {
+
+        // sort array
         arr.sort((a, b) => a - b);
 
+        // take two smallest ropes
         let first = arr.shift();
         let second = arr.shift();
 
+        // connect them
         let sum = first + second;
 
+        // add to total cost
         cost += sum;
 
+        // push new rope
         arr.push(sum);
     }
 
